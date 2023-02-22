fbd8465f9f3cc2954143aacd04f3a9565b65f349
Andrew Chaa <andrew.yh.chaa@gmail.com>
2023-02-22 23:59:32 +0000

    Removed console

--- src/isPalindrome.ts
+++ src/isPalindrome.ts
@@ -3,8 +3,6 @@ export default function isPalindrome(s: string) {
   let end = s.length - 1;
 
   while (s[start] === s[end]) {
-    console.log('start', start, 'end', end);
-
     if (Math.abs(start - end) <= 1) {
       return true;
     }
